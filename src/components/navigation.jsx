abe7e70f675681893f636c8a0054a705f594547c
Zach G <Zach.the.gecko@gmail.com>
2020-09-17 15:37:18 -0600

    Added icons instead of words when screen gets small

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -3,57 +3,61 @@ import { connect } from "react-redux";
 import { auth } from "../firebase/firebase.utils";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+
 import GroupChatIcon from "../static/group-chat-icon.png";
 import PrivateChatIcon from "../static/private-chat-icon.png";
+import HomeIcon from "../static/home-icon.png";
+import SignOutIcon from "../static/sign-out-icon.png";
 
-// Going to add Icons instead of words when screen gets smaller
+// Note to Self --> Going to add Icons instead of words when screen gets smaller
 
 const NavigationBar = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background: #212121;
-  color: #fff;
+  background: #fff;
+  color: #212121;
   padding: 1em 2em;
 `;
 const NavigationHeading = styled.span`
   font-size: 2em;
   font-weight: 100;
-  margin-right: 5em;
 `;
 const NavigationText = styled(Link)`
   font-size: 1.5em;
   cursor: pointer;
   text-decoration: none;
-  color: #fff;
+  color: #212121;
   &::before {
     content: ${(props) => `"${props.text}"`};
+    color: ${(props) => (props.signOut ? "Tomato" : "")};
   }
-  @media only screen and (max-width: 60000px) {
+  @media only screen and (max-width: 1000px) {
     &::before {
       content: ${(props) => `url("${props.img}")`};
+      padding: 10px;
     }
   }
-`;
-const SignOut = styled.span`
-  color: tomato;
-  font-size: 1.5em;
-  cursor: pointer;
 `;
 
 const Navigation = ({ currentUser }) => {
   return (
     <NavigationBar>
       <NavigationHeading>Hello, {currentUser.displayName}</NavigationHeading>
-      <NavigationText to="/home" />
+      <NavigationText to="/home" text="Home" img={HomeIcon} />
       <NavigationText to="/group-chat" img={GroupChatIcon} text="Group Chat" />
       <NavigationText
         to="/private-chat"
         img={PrivateChatIcon}
         text="Private Chats"
       />
-      <SignOut onClick={() => auth.signOut()}>Sign Out</SignOut>
+      <NavigationText
+        onClick={() => auth.signOut()}
+        text="Sign Out"
+        img={SignOutIcon}
+        signOut
+      />
     </NavigationBar>
   );
 };
